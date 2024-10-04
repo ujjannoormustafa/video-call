79a2d858280b5bc668a298b23cb5e508f3661e5c
ujjannoormustafa <ujjannoormustafa123@gmail.com>
2024-10-05 00:28:57 +0500

    fix

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ io.on('connection', (socket) => {
 });
 
 // Listening on PORT
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
